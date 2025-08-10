60d7418c0973a67903c90d9dda8106ac903573b0
brunkeralf <brunkeralf@gmail.com>
2025-08-10 16:12:16 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 22 }}>LagerGTP5 – Start</Text>
+      <Text>Basapp på plats ✅</Text>
+    </SafeAreaView>
+  );
+}
